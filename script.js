ea65d5699e7b4be766c0da69fae35f2852f3a193
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-05 18:54:24 +0530

    final commit
    Now the app is working fine ,sticky nav is coming when scrolling to bottom .

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const nav = document.querySelector("#main")
+const topOfNav = nav.offsetTop;         //distance of top from nav
 
 
 
 function fixNav() {
-    const topOfNav = nav.offsetTop;         //distance of top from nav
-    const distNav = window.scrollY          // Distance of scrolled screen
-     console.log(topOfNav ,distNav );      
+    //  window.scrollY        ===  Distance of scrolled screen
+     console.log(topOfNav ,window.scrollY );      
+
+    if(  window.scrollY  >= topOfNav ){
+        // console.log("match");
+        document.body.style.paddingTop = nav.offsetHeight + "px";
+        document.body.classList.add("fixed-nav");
+    }else{
+        document.body.classList.remove("fixed-nav");
+    }
 }
 
 // console.log(nav);
